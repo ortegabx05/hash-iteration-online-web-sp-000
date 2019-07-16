afdb76a1106ed2bd9763f49beba30ea8e3ba2be3
jessi ortega <ortegabx05@gmail.com>
2019-07-16 19:07:18 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,6 +15,8 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |age|
     if i[:age] < 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    else puts"You are too old for this."
- end
+    else
+      puts"You are too old for this."
+    end
+  end
 end
